19464150573bf644a873486e53c815331c2b075b
azer <benazzouzaze@gmail.com>
2023-10-01 13:05:43 +0100

    Exercice 8 At.1 ✔

--- At2/ex8.php
+++ At2/ex8.php
@@ -8,6 +8,24 @@ impair. -->
     <title>exercise 8️⃣</title>
 </head>
 <body>
-    
+    <form action="ex8.php">
+        <input type="number" name="num" id="num" value="0">
+        <input type="submit" value="check">
+    </form>
 </body>
 </html>
+<?php
+    if(!isset($_GET["num"])){
+        exit();
+    }
+    $num = $_GET["num"];
+    echo "<script>let f1 = document.forms[0];
+            f1.num.value = $num;
+        </script>";
+    $num == '' ? $num = 0 : $num = $num;
+    if($num % 2 == 0){
+        echo "<h1>$num is even</h1>";
+    }else{
+        echo "<h1>$num is odd</h1>";
+    }
+?>
