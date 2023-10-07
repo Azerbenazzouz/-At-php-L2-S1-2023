e7f7c49bc8b206ddbcf6eb8d3bf6524ac0ec39ac
Azer Ben Azzouz <benazzouzazer@gmail.com>
2023-10-07 11:26:08 +0100

    At2 ex2 ❤❤✨

--- At2/Ex2.php
+++ At2/Ex2.php
@@ -3,9 +3,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="./bootstrap.css">
     <title>Ex2</title>
 </head>
 <body>
-    
+    <div class="container mt-3">
+        <?php 
+            echo "<h1 class='text-center text-primary'>Exercice 2 :</h1><br>";
+            $tabpays1 = array("dubai", "italie", "allemagne", "espagne","suisse" , "tunisie");
+
+            // afficher le tableau
+
+            echo "<h1 >Tableau des pays :</h1><br>";
+            echo "<table class='table table-hover'>
+                <tr>
+                    <th>Index</th>
+                    <th>Pays</th>
+                </tr>";
+            foreach ($tabpays1 as $key => $value) {
+                echo "<tr class='table-active'>
+                        <td>$key</td>
+                        <td>$value</td>
+                    </tr>";
+            }
+            echo "</table>";
+            // trie du tableau croissant ordre alphabetique
+            echo "<h1 >Tableau des pays trié par ordre alphabetique :</h1><br>";
+            sort($tabpays1);
+            echo "<table class='table table-hover'>
+                <tr>
+                    <th>Index</th>
+                    <th>Pays</th>
+                </tr>";
+            foreach ($tabpays1 as $key => $value) {
+                echo "<tr class='table-active'>
+                        <td>$key</td>
+                        <td>$value</td>
+                    </tr>";
+            }
+            echo "</table>";
+            // trie du tableau decroissant ordre valeur
+            arsort($tabpays1);
+            echo "<h1 >Tableau des pays trié par ordre valeur :</h1><br>";
+            echo "<table class='table table-hover'>
+                <tr>
+                    <th>Index</th>
+                    <th>Pays</th>
+                </tr>";
+            foreach ($tabpays1 as $key => $value) {
+                echo "<tr class='table-active'>
+                        <td>$key</td>
+                        <td>$value</td>
+                    </tr>";
+            }
+            echo "</table>";
+        ?>
+    </div>
 </body>
 </html>
--- At2/index.php
+++ At2/index.php
@@ -9,7 +9,7 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-light" data-bs-theme="light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">Navbar</a>
+        <a class="navbar-brand" href="/">Atelier 2</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
